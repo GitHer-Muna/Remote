ee4aa0ab58259fc309ee8d39313d2dd69a293b17
GitHer-Muna <muna.bhattarai@study.lbef.edu.np>
2024-06-25 22:40:35 +0545

    Add prime number check to helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -13,4 +13,19 @@ elif num % 2 == 0:
     print("The number is even")
 else:
     print("The number is odd")
-    
+
+#also test for prime numbers
+num = int(input("Enter a number: "))
+if num < 0:
+    print("The number is negative")
+elif num == 0:
+    print("The number is zero")
+elif num == 1:
+    print("The number is one")
+else:
+    for i in range(2, num):
+        if num % i == 0:
+            print("The number is not prime")
+            break
+    else:
+        print("The number is prime")
